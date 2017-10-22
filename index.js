d053d3ea31a4136516b058765e455679f7dd8c39
Brandon Wood <woodb@users.noreply.github.com>
2017-10-22 12:54:42 -0500

    ENH: Catch errors, output traces
    Additionally, added some documentation and code cleanups.

--- index.js
+++ index.js
@@ -2,39 +2,45 @@
 const readline = require("readline");
 const cli = require("commander");
 
-const defaultFn = obj => {
-  return obj;
-};
+
+/* Default function if no code is specified in the command line */
+const defaultFn = obj => obj;
 
 
 /*
  * Process input stream, aggregating into a single string, then parse and
  * process
  */
-const processJson = (fn=defaultFn) => {
+const processJson = fn => {
   const raw = [];
   readline
     .createInterface({
       input: process.stdin,
     })
-    .on("line", line => {
-      raw.push(line);
-    })
+    .on("line", line => raw.push(line))
     .on("close", () => {
-      const rawJson = raw.join("");
-      const obj = JSON.parse(rawJson);
-      const rv = fn(obj);
-      console.log(JSON.stringify(rv, null, 2));
-      process.exit(0);
+      // Try to parse the JSON
+      try {
+        const parsedJson = JSON.parse(raw.join(""));
+        const rv = fn(parsedJson);
+        console.log(JSON.stringify(rv, null, 2));
+        process.exit(0);
+      } catch (err) {
+        console.error(err);
+        process.exit(1);
+      }
     });
 };
 
 
+/* Process command line arguments */
 cli
   .version("0.0.1")
   .option("-c, --code <code>", "Function to execute to parse JSON");
-
 cli.parse(process.argv);
 
+
+/* Create stdin stream processor and execute */
 const { code } = cli;
-processJson(eval(code));
+const fn = code ? eval(code) : defaultFn;
+processJson(fn);
