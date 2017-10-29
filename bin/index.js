68b85d37fbde700d1e3e143fc3df52ba5fe72b8a
Brandon Wood <woodb@users.noreply.github.com>
2017-10-29 13:27:33 -0500

    ENH: Refactored functions to be easier to test

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 const cli = require("commander");
-const { processJson } = require("../src");
-
+const { main } = require("./main");
 
 /* Process command line arguments */
 cli
-  .version("0.0.2")
+  .version("0.1.0-alpha.0")
   .option("-c, --code <code>", "Function to execute to parse JSON");
 cli.parse(process.argv);
 
 
-/* Create stdin stream processor and execute */
-const { code } = cli;
-const fn = code ? eval(code) : void 0;
-processJson(fn);
+const run = async () => {
+  try {
+    console.log(await main(cli, process.stdin));
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+run();
--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 const readline = require("readline");
 
 
-/* Default function if no code is specified in the command line */
-const defaultFn = obj => obj;
-
-
-/*
+/**
  * Process input stream, aggregating into a single string, then parse and
  * process
  */
-const processJson = (fn = defaultFn) => {
-  const raw = [];
-  readline
-    .createInterface({ input: process.stdin })
-    .on("line", line => raw.push(line))
-    .on("close", () => {
-      try {
-        const parsedJson = JSON.parse(raw.join(""));
-        const rv = fn(parsedJson);
-        console.log(JSON.stringify(rv, null, 2));
-        process.exit(0);
-      } catch (err) {
-        console.error(err);
-        process.exit(1);
-      }
-    });
-};
+const processJsonBuffer = ({ buffer = process.stdin, fn = obj => obj }) =>
+  new Promise((resolve, reject) => {
+    const raw = [];
+    readline
+      .createInterface({ input: buffer })
+      .on("line", line => raw.push(line))
+      .on("close", () => {
+        try {
+          const parsedJson = JSON.parse(raw.join(""));
+          const rv = fn(parsedJson);
+          resolve(rv);
+        } catch (err) {
+          reject(err);
+        }
+      });
+  });
 
 
 module.exports = {
-  processJson,
+  processJsonBuffer,
 };
